6c4207f260575956c7d1c80cbdb6c76ffd6a75f0
aidai <makhmatovaaidai@gmail.com>
2022-10-12 20:31:29 +0600

    first name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Comment from "./components/Comment";
+
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl:
+      "https://www.petage.com/wp-content/uploads/2019/09/Depositphotos_74974941_xl-2015-e1569443284386-670x627.jpg",
+  },
+};
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Comment data={comment} />
+    </div>
+  );
 }
 
 export default App;
+
